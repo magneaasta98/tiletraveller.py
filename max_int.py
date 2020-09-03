21a006bfa13afa50b63f18e2ed656d4b7866b72d
Helga Þórey <helgathorey@Helgas-MacBook.lan>
2020-09-03 14:19:36 +0000

    Add new feature

---
+++ max_int.py
@@ -0,0 +1,8 @@
+num_int=1
+max_int=0
+while num_int>0:
+    num_int = int(input("Input a number: "))
+    if num_int>max_int:
+        max_int=num_int
+
+print("The maximum is", max_int)    # Do not change this line
