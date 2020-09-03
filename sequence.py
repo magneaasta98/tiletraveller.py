3fafa003d315f24afd4d370d4b385da477a2ea3a
Helga Þórey <helgathorey@Helgas-MacBook.lan>
2020-09-03 15:13:11 +0000

    uppfærsla

--- sequence.py
+++ sequence.py
@@ -1,5 +1,13 @@
 n = int(input("Enter the length of the sequence: "))
+n1=1
+n2=2
+n3=3
+for num in range(1,n+1):
+    n1=num(1)
+    print(n1)
 
-range(1,5)
-#for number in range(1,n+1):
-   
+
+
+
+
+
